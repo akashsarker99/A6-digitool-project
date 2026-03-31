5de754f2028908073107873cfe256f38d260aa7b
akashsarker99 <akashsarker2003@gmail.com>
2026-03-31 09:19:45 +0600

    Navbar created with the help of DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   
   return (
     <>
-      <h1 className='btn'>heloooooooooooooooooooooo</h1>
+      <Navbar></Navbar>
     </>
   )
 }
