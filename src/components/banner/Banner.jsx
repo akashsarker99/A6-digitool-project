2704a72ef926c08a03bef2cece0a20fabc5624ad
akashsarker99 <akashsarker2003@gmail.com>
2026-03-31 11:58:11 +0600

    stats Section created, Banner and stats made responsive

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,28 +5,28 @@ import { CiPlay1 } from 'react-icons/ci';
 const Banner = () => {
     return (
         <div>
-            <div className="hero w-11/12 mx-auto mt-8">
+            <div className="hero w-11/12 mx-auto my-8">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img
       src={bannerimg}
-      className="max-w-sm rounded-lg"
+      className="max-w-sm rounded-lg w-[80%]"
     />
     <div className=' space-y-3'>
         
-            <h2 className='bg-[#E1E7FF] w-11/12 md:w-[300px] text-center p-2 rounded-full font-semibold text-[#4F39F6] flex items-center justify-center gap-2'><FaDotCircle></FaDotCircle> New: AI-Powered Tools Available</h2>
-      <h1 className="text-5xl font-bold leading-tight">Supercharge Your<br></br>Digital Workflow</h1>
-      <p className='text-[#627382]' >
+            <h2 className='bg-[#E1E7FF] w-10/12 text-center p-2 rounded-full font-semibold text-[#4F39F6] flex items-center justify-center gap-2 text-sm sm:text-lg'><FaDotCircle></FaDotCircle> New: AI-Powered Tools Available</h2>
+      <h1 className="text-4xl md:text-5xl font-bold leading-tight">Supercharge Your<br></br>Digital Workflow</h1>
+      <p className='text-[#627382] text-xs sm:text-lg' >
        Access premium AI tools, design assets, templates, and productivity<br></br>
 software—all in one place. Start creating faster today.<br></br>
 
 Explore Products
 
       </p>
-     <div className='flex gap-3'>
-         <button className="btn text-white font-bold bg-linear-65 from-[#4F39F6] to-[#9514FA] rounded-full transition-all ease-in-out duration-400 hover:scale-105">Explore Products</button>
-         <div className='p-[1px] rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] inline-block'>
+     <div className='flex gap-3 flex-col sm:flex-row'>
+         <button className="btn text-white font-bold bg-linear-65 from-[#4F39F6] to-[#9514FA] rounded-full transition-all ease-in-out duration-400 hover:scale-105 btn-sm sm:btn-md mx-auto sm:mx-0">Explore Products</button>
+         <div className='p-[1px] rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] inline-block btn-sm sm:btn-md mx-auto sm:mx-0'>
 
-        <button className="btn font-bold border-0 text-[#4F39F6] rounded-full hover:bg-linear-to-r from-[#4F39F6] to-[#9514FA] hover:text-white transition-all ease-in-out duration-400 hover:scale-105"><FaPlay></FaPlay> Watch Demo</button>
+        <button className="btn font-bold border-0 text-[#4F39F6] rounded-full hover:bg-linear-to-r from-[#4F39F6] to-[#9514FA] hover:text-white transition-all ease-in-out duration-400 hover:scale-105 btn-sm sm:btn-md"><FaPlay></FaPlay> Watch Demo</button>
          </div>
      </div>
     </div>
--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -3,8 +3,20 @@ import React from 'react';
 const Stats = () => {
     return (
         <div>
-            <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA]'>
-                <h3>dfsfsfsdfsdfsdfsfs</h3>
+            <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-15 grid grid-cols-3'>
+                <div>
+                    <h1 className='text-5xl font-extrabold text-white mb-2'>50K+</h1>
+                    <p className='text-white opacity-80'>Active Users</p>
+                </div>
+                <div>
+                    <h1 className='text-5xl font-extrabold text-white mb-2'>50K+</h1>
+                    <p className='text-white opacity-80'>Active Users</p>
+                </div>
+                <div>
+                    <h1 className='text-5xl font-extrabold text-white mb-2'>50K+</h1>
+                    <p className='text-white opacity-80'>Active Users</p>
+                </div>
+               
             </div>
         </div>
     );
